d2ebbaea60afdfbc84d6d8daeae169df0a41ab16
CrisVOJ <cristopheroliverajimenez@gmail.com>
2024-07-25 10:32:10 -0400

    Agregando titulos a operaciones

--- test.py
+++ test.py
@@ -1,11 +1,11 @@
 def suma(a, b):
-    print(a+b)
+    print(f'Suma: {a} + {b} = {a+b}')
 
 def resta(a, b):
-    print(a-b)
+    print(f'Resta: {a} - {b} = {a-b}')
 
 def multiplicacion(a, b):
-    print(a*b)
+    print(f'Multiplicacion: {a} x {b} = {a*b}')
 
 suma(1, 2)
 resta(1, 2)
